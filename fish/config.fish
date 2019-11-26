2a8d9b315b328b71a4db3b7cccf92ce513aa51b3
Chad Seeger <90306+cseeger@users.noreply.github.com>
2019-11-26 14:42:56 -0800

    backup fish config (email)

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+. "$HOME/.config/fish/functions/aliases.fish"
+
+begin
+  set --local AUTOJUMP_PATH $HOME/.config/fish/functions/autojump.fish
+  if test -e $AUTOJUMP_PATH
+      source $AUTOJUMP_PATH
+  end
+end
+
+status --is-interactive; and source (rbenv init -|psub)
+
+export NVM_DIR="$HOME/.nvm"
+fzf_key_bindings
---
+++ fish/functions/aliases.fish
@@ -0,0 +1,18 @@
+alias l="ls -las"
+
+# Git
+alias g="git"
+alias grhh="git reset HEAD --hard"
+alias gd="git diff"
+alias gst="git status"
+alias gco="git checkout"
+alias gpom="git pull origin master"
+
+# Quick check running state of a process
+alias psef="ps -ef | peco "
+alias top="top -o cpu"
+
+# Rails
+alias be="bundle exec"
+alias rs="bundle exec rails s"
+alias rc="bundle exec rails c"
